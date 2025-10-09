b2a00d8b4cf3510885ce07697795e1b251373e4b
AnuradhaUpar <anuradha.upar11@gmail.com>
2025-10-09 12:39:03 +0530

    Day 32: completed project.

--- backend.py
+++ backend.py
@@ -7,13 +7,9 @@ def get_data(place,days=None,kind=None):
     response = requests.get(url)
     data = response.json()
     filtered_data = data['list']
-    filtered_data = filtered_data[0:8*days]
-    if kind == 'Temperature':
-        filtered_data = [dict['main']['temp'] for data in filtered_data]
-    if kind == "sky":
-        filtered_data = [dict["weather"]["main"] for data in filtered_data]
-
+    nr_days =8*days
+    filtered_data = filtered_data[0:nr_days]
     return filtered_data
 
 if __name__ == "__main__":
-    print(get_data(place="Thane"))
+    print(get_data(place="Thane",days=1,kind='sky'))
--- main.py
+++ main.py
@@ -3,7 +3,7 @@ import plotly.express as px
 from backend import get_data
 
 #express is module, plotly is library
-
+#Add titile, text input ,slider,selectionbox and subheader
 st.title("Weather Forcast for Next Days ")
 place = st.text_input("Place")
 
@@ -12,22 +12,24 @@ days = st.slider("Forcast Days",min_value=1,max_value=5,
 option = st.selectbox("select data to view",
                       ("Temperature","Sky"))
 st.subheader(f"{option} for next {days} days in {place}")
-
-data= get_data(place,days,option)
-
-
-
-    dates=['2022-25-10','2022-26-10','2022-27-10']
-    tempratures=[10,11,15]
-    tempratures=[days * i for i in tempratures]
-    return dates,tempratures
-#figure object
-
-d, t=get_Data(days)
-
-figure = px.line(x=d , y=t  ,labels={"x": "Date","y":"Tempraturs(C)"})
-st.plotly_chart(figure)
-#plotly_chart() is a method responsible to create a graph
-# and this method now gets a figure object as input
-#figure object you can get it from plotting library,from a data visualization library
-#plotly is data visualization library, such as plotly orBokeh
+if place:
+#get the temperature/sky day
+    filtered_data = get_data(place,days)
+    #print(filtered_data)
+    if option == "Temperature":
+    #create a temperature plot
+        temperatures = [dict['main']['temp'] /10  for dict in filtered_data]; print("temp=",temperatures)
+        dates = [dict['dt_txt'] for dict in filtered_data];print(dates)
+        figure = px.line(x=dates , y=temperatures  ,labels={"x": "Date","y":"Temperaturs(C)"})
+        st.plotly_chart(figure)
+        #plotly_chart() is a method responsible to create a graph
+        # and this method now gets a figure object as input
+        #figure object you can get it from plotting library,from a data visualization library
+        #plotly is data visualization library, such as plotly orBokeh
+    if  option == "Sky":
+        img_dic = {'Clear': "images/clear.png", 'Cloud': "images/cloud.png", 'Rain': "images/rain.png",
+                   'Snow': "images/snow.png"}
+        sky_conditions= [dict['weather'][0]['main'] for dict in filtered_data]
+        image_path=[img_dic[condition] for condition in sky_conditions]
+        print(sky_conditions)
+        st.image(image_path,width=115)
